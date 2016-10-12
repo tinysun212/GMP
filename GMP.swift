fc237cde9b3421be7521999ad19d3b08ee7d7417
Han Sangjin <tinysun.net@gmail.com>
2016-10-12 02:38:30 -0400

    Now working module
    - Changed CGMP URL
    - Added public keyword

--- GMP.swift
+++ GMP.swift
@@ -1,64 +1,64 @@
 import CGMP
 
-class GMP {
+public class GMP {
 	var v_ = mpz_t()
-	init() {
+	public init() {
 		__gmpz_init(&v_)
 	}
 
-	init(n: UInt) {
+	public init(n: UInt) {
 		__gmpz_init_set_ui(&v_, n)
 	}
 
-	init(n: mpz_t) {
+	public init(n: mpz_t) {
 		v_ = n
 	}	
 
-	func setValue(n: UInt) {
+	public func setValue(n: UInt) {
 		__gmpz_set_ui(&v_, n)
 	}
 
-	func add(n: UInt) {
+	public func add(n: UInt) {
 		__gmpz_add_ui(&v_, &v_, n)
 	}
 
-	func mul(n: UInt) {
+	public func mul(n: UInt) {
 		__gmpz_mul_ui(&v_, &v_, n)
 	}
 
-	func mul(n: GMP) {
+	public func mul(n: GMP) {
 		__gmpz_mul(&v_, &v_, &n.v_)
 	}
 
-	func mod(n: GMP) {
+	public func mod(n: GMP) {
 		__gmpz_mod(&v_, &v_, &n.v_)
 	}
 
-	func compare(n: UInt) -> Int32 {
+	public func compare(n: UInt) -> Int32 {
 		return __gmpz_cmp_ui(&v_, n)
 	}
 
-	func compare(v2: GMP) -> Int32 {
+	public func compare(v2: GMP) -> Int32 {
 		return __gmpz_cmp(&v_, &v2.v_)
 	}
 
-	func assignSub(v1: GMP, v2: GMP) {
+	public func assignSub(v1: GMP, v2: GMP) {
 		__gmpz_sub(&v_, &v1.v_, &v2.v_)
 	}
 
-	func assignGCD(v1: GMP, v2: GMP) {
+	public func assignGCD(v1: GMP, v2: GMP) {
 		__gmpz_gcd(&v_, &v1.v_, &v2.v_)
 	}
 
-	func set(n: GMP) {
+	public func set(n: GMP) {
 		__gmpz_set(&v_, &n.v_)
 	}
 
-	func assignPow(base: UInt, p: UInt) {
+	public func assignPow(base: UInt, p: UInt) {
 		__gmpz_ui_pow_ui(&v_, base, p)
 	}
 
-	func toString() -> String {
+	public func toString() -> String {
 		let string = String(cString:__gmpz_get_str(nil, 10, &v_))
 		return  string
 	}
--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "GMP",
     dependencies: [
-        .Package(url: "../CGMP", majorVersion: 1, minor: 2)
+        .Package(url: "https://github.com/tinysun212/CGMP.git", majorVersion: 1, minor: 2)
     ]
 )
